9be88a6644ea7026c38d500c7cfdcab0cfa0f0ee
Gourab Bistu <gourabbistu2002@gmail.com>
2026-01-13 15:36:53 +0530

    iniital setup of frontend and backend with minimal folder structure

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+const PORT = 5000;
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("🚀 Express server is running");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
